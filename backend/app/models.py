713d8dd397d579990f670dfff427ab9001ae6711
Petros-Code <petros-code@proton.me>
2025-10-31 12:02:07 +0100

    Merge crud dans la main

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Boolean
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 
